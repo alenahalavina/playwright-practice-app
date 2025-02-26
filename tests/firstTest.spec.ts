23df84c70071e7cd7f1d6cbce083a43f350acaea
Alena Halavina <alenahalavin@gmail.com>
2025-02-25 21:22:17 -0800

    Extracting values

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -88,5 +88,28 @@ test('Reusing the locators', async({page}) =>{
     await basicForm.locator('nb-checkbox').click()
     await basicForm.getByRole('button').click()
 
-   await expect(emailField).toHaveValue('test@test.com')     
+    await expect(emailField).toHaveValue('test@test.com')     
+})
+
+test('Extracting values', async({page}) => {
+    //single text value
+    const basicForm = page.locator('nb-card').filter({hasText: 'Basic form'})
+    const buttonText =  await basicForm.locator('button').textContent()
+
+    expect(buttonText).toEqual('Submit') 
+
+    //all text values
+    await page.locator('nb-radio').allTextContents()
+    const allRadioButtonLabels = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonLabels).toContain('Option 1') 
+
+    //input value
+    const emailField = basicForm.getByRole('textbox', {name:'Email'})
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    const placeholderValue = await emailField.getAttribute('placeholder')
+    expect (placeholderValue).toEqual('Email')
+
 })
