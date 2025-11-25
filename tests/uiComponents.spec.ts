a7d7cd5893496621366ac0d3aa0d57ba4444fbde
Alena Halavina <alenahalavin@gmail.com>
2025-11-25 00:02:47 -0800

    Added radio buttons test

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -25,4 +25,20 @@ test.describe('Forms Layouts page', () => {
         await expect(usingTheGridEmailInput).toHaveValue('test@test.com')        
     })
 
+    test('radio buttons', async({page}) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+        
+        //await usingTheGridForm.getByLabel('Option 1').check({force: true})
+        await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).check({force: true})
+        const readioStatus = await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()
+        expect(readioStatus).toBeTruthy()
+        await expect(usingTheGridForm.getByRole('radio', {name: 'Option 1'})).toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).check({force: true})
+        expect(await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
+        
+        
+    })
+
 })
