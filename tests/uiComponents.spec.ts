868ee3fc5882c647a82ceeab5d1117c4e04916f3
Alena Halavina <alenahalavin@gmail.com>
2025-11-25 00:34:08 -0800

    Added check boxes lesson

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -39,6 +39,24 @@ test.describe('Forms Layouts page', () => {
         expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
         
         
-    })
 
+})
+
+})
+
+    test('check boxes', async({page}) => {
+        await page.getByText('Modal & Overlays').click() 
+        await page.getByText('Toastr').click()
+
+        await page.getByRole('checkbox', {name: 'Hide on click'}).uncheck({force: true})
+        await page.getByRole('checkbox', {name: 'Prevent arising of duplicate toast'}).check({force: true})
+
+        const allBoxes = page.getByRole('checkbox')
+        for(const box of await allBoxes.all()){
+            await box.check({force: true})
+            expect(await box.isChecked()).toBeTruthy()
+        }
+
+
+        
 })
